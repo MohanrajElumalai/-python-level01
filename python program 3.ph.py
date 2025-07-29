ed9e319d3372f4ab6752260021c5b7c85f9abd74
MohanrajElumalai <maybemohan8899@gmail.com>
2025-07-29 10:10:42 +0530

    mohan

---
+++ python program 3.ph.py
@@ -0,0 +1,19 @@
+print("goverment of tamilnadu")
+print("elecity broad")
+print("....................")
+no1=input("enter the eb no")
+no2=input("enter the name")
+no3=int(input("readind the previous:"))
+no4=int(input("reading the current month:"))
+total=no4-no3
+print("total unit consumed:",total)
+paid=total*5
+print("amount to be paid rs:",paid)
+
+
+
+
+
+ 
+ 
+ 
