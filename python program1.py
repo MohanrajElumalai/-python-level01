b4be29b8d324c122a8ba3a16bc2d1557dc2a7299
MohanrajElumalai <maybemohan8899@gmail.com>
2025-07-29 10:08:06 +0530

    Add files via upload

---
+++ python program1.py
@@ -0,0 +1,19 @@
+print("tvk:")
+print("ongur ,mathur village:")
+print("......................:")
+print("student mark list:")
+print("...................:")
+str1=(input("enter the reg no:"))
+str2=(input("enter the name:"))
+m1=int(input("python mark:"))
+m2=int(input("dbms mark:"))
+m3=int(input("acc mark:"))
+total=m1+m2+m3
+print("total mark:",total)
+avg=total/3
+print("avg mark:",avg)
+
+
+
+
+
