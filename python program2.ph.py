fa120568a3267008e9c1022a2cc099110874f102
MohanrajElumalai <maybemohan8899@gmail.com>
2025-07-29 10:25:46 +0530

    mohan

---
+++ python program2.ph.py
@@ -0,0 +1,15 @@
+print("ARITHMATIC OPEATIONS")
+print("--------------")
+a=int(input("enter the frist number:"))
+b=int(input("enter the second number:"))
+print("result:")
+c=a+b
+print("add:",c)
+d=a-b
+print("sud:",d)
+e=a*b
+print("mult:",e)
+f=a/b
+print("div:",f)
+g=a%b
+print("module:",g)
