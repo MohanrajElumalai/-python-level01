de7a03c510cc9d14fc8203b4952880b3f862a162
MohanrajElumalai <maybemohan8899@gmail.com>
2025-07-29 10:11:22 +0530

    mohan

---
+++ python program 4.ph.py
@@ -0,0 +1,22 @@
+print("MOHANRAJ SUPER MARKET")
+print("no,22 mathur village permual kovil street")
+print("-------------------------")
+print("bill receipt")
+print("--------------")
+input("Enter the serial no")
+input("Enter the particular:")
+rate=int(input("Enter the rate:"))
+quantity=int(input("Enter the qultly:"))
+Total=rate*quantity
+print("Total amount:",Total)
+gst=Total*10/100
+print("gst (10PER):",gst)
+print("amount to be paid:",Total)
+
+
+
+
+
+ 
+ 
+ 
